145e79e7db98361d18ee0f010ead671fa8daa6f2
Taishi Kusunose <taishi.kusunose@gmail.com>
2021-11-18 08:19:10 +0900

    add-totalCalculate

--- indiKadai1/ViewController.swift
+++ indiKadai1/ViewController.swift
@@ -6,13 +6,35 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
 
+    @IBOutlet private weak var firstTextField: UITextField!
+    @IBOutlet private weak var secondTextField: UITextField!
+    @IBOutlet private weak var thirdTextField: UITextField!
+    @IBOutlet private weak var fourthTextField: UITextField!
+    @IBOutlet private weak var fifthTextField: UITextField!
+    
+    @IBOutlet weak var resultLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        resultLabel.text = "0"
     }
 
-
+    @IBAction private func calcAction(_ sender: Any) {
+        resultLabel.text = "\(totalCalculate())"
+    }
+    
+    private func totalCalculate() -> Int {
+        let inputArray: [UITextField] = [
+            firstTextField,
+            secondTextField,
+            thirdTextField,
+            fourthTextField,
+            fifthTextField
+        ]
+        return inputArray
+            .map({Int($0.text ?? "0") ?? 0})
+            .reduce(0,+)
+    }
 }
-
